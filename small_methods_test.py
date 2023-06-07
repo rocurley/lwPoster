97ce8e5d536159bc86d3a6220a64985ca7770d62
Jacob Kopczynski <ja.kopczynski@gmail.com>
2023-06-06 21:09:32 -0700

    next weekday more cases

--- small_methods_test.py
+++ small_methods_test.py
@@ -12,5 +12,19 @@ class TestNextWeekday(unittest.TestCase):
         self.assertEqual(apis.next_weekday(monday_noon, target_number), tuesday_noon)
         print("correct weekday")
 
+    def test_tuesday_monday(self):
+        tuesday_noon = datetime.datetime(2023, 5, 2, 12, 0, 0) #May 2 2023 is a Tuesday
+        next_monday_noon = datetime.datetime(2023, 5, 8, 12, 0, 0) # 6*24 hours later exactly
+        target_number = 0 # Monday
+        self.assertEqual(apis.next_weekday(tuesday_noon, target_number), next_monday_noon)
+        print("correct weekday in next week")
+
+    def test_tuesday_saturday(self):
+        tuesday_noon = datetime.datetime(2023, 5, 2, 12, 0, 0) #May 2 2023 is a Tuesday
+        next_saturday_noon = datetime.datetime(2023, 5, 6, 12, 0, 0) # 4*24 hours later exactly
+        target_number = 5 # Monday
+        self.assertEqual(apis.next_weekday(tuesday_noon, target_number), next_saturday_noon)
+        print("correct weekday late in week")
+
 if __name__ == '__main__':
     unittest.main()
